b77ba2289c3e378895585157099089d914ef5138
Feysel Sied <feiselseid123@gmail.com>
2025-12-24 11:27:07 +0300

    Implemented basic accordion feature.

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
 
 
 
-console.log("I am javascript:)");
+const accordionButtons = document.querySelectorAll('.question');
+console.log(accordionButtons);
+
+accordionButtons.forEach((button) => {
+    button.addEventListener('click', function() {
+        this.nextElementSibling.classList.toggle('hide');
+    } )
+});
